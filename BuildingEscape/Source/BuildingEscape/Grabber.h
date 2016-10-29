adaf16984af25232594400c6d393c11fb349a5f9
Zeno <amarzeno@outlook.com>
2016-10-29 16:18:41 -0600

    Added debug line tracing

--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -21,6 +21,7 @@ public:
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
-		
+	// How far ahead pf the player can we reach in cm
+	float Reach = 100.f;
 	
 };
