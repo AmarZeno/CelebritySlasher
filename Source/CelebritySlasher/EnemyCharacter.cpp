a1aa9e3102728506db095bdc453f07d3625bdc82
Zeno <amarzeno@outlook.com>
2016-11-05 21:32:07 -0600

    Screen feedback work in progress

--- Source/CelebritySlasher/EnemyCharacter.cpp
+++ Source/CelebritySlasher/EnemyCharacter.cpp
@@ -33,3 +33,5 @@ void AEnemyCharacter::SetupPlayerInputComponent(class UInputComponent* InputComp
 
 }
 
+
+
---
+++ Source/CelebritySlasher/ScreenFeedback.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CelebritySlasher.h"
+#include "ScreenFeedback.h"
+
+// Sets default values for this component's properties
+UScreenFeedback::UScreenFeedback()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	CurrentHealth = 100;
+	MaxHealth = 100;
+}
+
+
+// Called when the game starts
+void UScreenFeedback::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+// Called every frame
+void UScreenFeedback::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
